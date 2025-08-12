31fa9d2f1a04d5cdc099273c22b954dfa0a2e15c
-ffix <164929254+f-fix@users.noreply.github.com>
2025-08-11 21:44:24 -0700

    Add files via upload

--- nontama_to_bload.py
+++ nontama_to_bload.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import unicodedata
 from functools import reduce
 
 NONTAMA_HEADER_START = b"\xffNONTAMA"
@@ -38,6 +39,8 @@ state.
 
 """
 
+IHEX_START=b'\r\n:'
+LOAD_NAME_PREFIX=b'Found:'
 
 def nontama_to_bload(b):
     """Extract NONTAMA-loader XOR'ed data from the P6/P6T tape
@@ -52,14 +55,50 @@ def nontama_to_bload(b):
         "<HHH", b[b.find(NONTAMA_HEADER_START) + len(NONTAMA_HEADER_START) :][:6]
     )
     assert start_addr < last_addr
-    assert start_addr <= exe_addr
     stop_addr = last_addr + 1
+    payload_start = b.find(NONTAMA_HEADER_START) + len(NONTAMA_HEADER_START) + 6
+    load_name = None
+    potential_loader = b[:b.find(NONTAMA_HEADER_START)]
+    if IHEX_START in potential_loader:
+        ihex = potential_loader[potential_loader.find(IHEX_START):]
+        ihex = ihex[:ihex.find(b'\0')]
+        ihex=ihex.rstrip(b'\x1A')
+        loader_payload = b""
+        loader_addr = None
+        for line in ihex.split(b'\r\n'):
+            if line.startswith(b':') and len(line) % 2 == 1:
+                try:
+                    int(line[1:], 16)
+                except:
+                    continue
+                data_bytes = int(line[1:3], 16)
+                if not data_bytes:
+                    break
+                ihex_addr = int(line[3:7], 16)
+                record_type = int(line[7:9], 16)
+                if record_type == 0x01:
+                    break
+                if record_type != 0x00:
+                    continue
+                if len(line) == 1 + 2 + 4 + 2 + 2 * data_bytes + 2:
+                    checksum_zeroing_byte = int(line[-2:], 16)
+                    ihex_data = bytes([int(line[1 + 2 + 4 + 2 + 2 * i:][:2], 16) for i in range(data_bytes)])
+                    if sum(int(line[i:i + 2], 16) for i in range(1, len(line), 2)) & 0xFF == 0x00:
+                        if loader_addr is None:
+                            loader_addr = ihex_addr
+                            loader_payload = ihex_data
+                        else:
+                            if ihex_addr < loader_addr:
+                                loader_payload = ihex_data + (b"\0" * (loader_addr - ihex_addr) + loader_payload)[len(ihex_data):]
+                                loader_addr = ihex_addr
+                            else:
+                                loader_payload = (loader_payload + (b"\0" * (ihex_addr - loader_addr)))[:(ihex_addr - loader_addr)] + ihex_data
+        if LOAD_NAME_PREFIX in loader_payload:
+            load_name = loader_payload[loader_payload.find(LOAD_NAME_PREFIX) + len(LOAD_NAME_PREFIX):].split(b'\0')[0].split(b'\n')[0].split(b'\r')[0].strip(b' \t') or None
     print(
-        f"NONTAMA start_addr=0x{start_addr:04X}, stop_addr=0x{stop_addr:04X}, exe_addr=0x{exe_addr:04X}"
+        f"NONTAMA start_addr=0x{start_addr:04X}, stop_addr=0x{stop_addr:04X}, exe_addr=0x{exe_addr:04X}, load_name={load_name}"
     )
-    ciphertext = b[b.find(NONTAMA_HEADER_START) + len(NONTAMA_HEADER_START) + 6 :][
-        : stop_addr - start_addr
-    ]
+    ciphertext = b[payload_start:][:stop_addr-start_addr]
     payload = reduce(
         lambda k_p, c: (c, k_p[1] + bytes([k_p[0] ^ c])),
         ciphertext,
@@ -67,25 +106,156 @@ def nontama_to_bload(b):
     )[1]
     return (
         struct.pack("<HH", start_addr, stop_addr) + payload,
+        load_name,
         start_addr,
         stop_addr,
         exe_addr,
+        b[payload_start+stop_addr-start_addr:],
     )
 
 
+NO_CONTROLS = b""
+MINIMAL_CONTROLS = b"\0\r\n\x1a\x7f"
+ASCII_CONTROLS = bytes(range(0x20)) + b"\x7f"
+
+# i am sure this is not the best way to solve this. this mapping
+# should work OK for PC-6001/mkII/SR and PC-6601/SR. it does not
+# handle the alternate character set shift sequences well. it also
+# does not handle Kanji or PC-6001A charset at all! the mapping is
+# intentionally close to the PC-98 one above. the hiragana and kanji
+# here should all be half-width ones, but Unicode is missing those so
+# we live with fullwidth instead.
+PC6001_8BIT_CHARSET = (
+    "␀␁␂␃␄␅␆␇␈␉␊␋␌␍␎␏␐␑␒␓␔␕␖␗␘␙␚␛￫￩￪￬"
+    " !\"#$%&'()*+,-./0123456789:;<=>?"
+    "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[¥]^_"
+    "`abcdefghijklmnopqrstuvwxyz{¦}~␡"
+    "♠♥♦♣￮•をぁぃぅぇぉゃゅょっーあいうえおかきくけこさしすせそ"
+    "\uf8f0｡｢｣､･ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿ"
+    "ﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝﾞﾟ"
+    "たちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわん\uf8f2\uf8f3"
+)
+assert len(PC6001_8BIT_CHARSET) == 256
+PC6001_8BIT_ALTCHARSET = "\uf8f1月火水木金土日年円時分秒百千万" "π┴┬┤├┼│─┌┐└┘╳大中小"
+assert len(PC6001_8BIT_ALTCHARSET) == 32
+PC6001_8BIT_CHARMAP = {PC6001_8BIT_CHARSET[i]: bytes([i]) for i in range(256)} | {
+    PC6001_8BIT_ALTCHARSET[i]: bytes([0x14, i + 0x30]) for i in range(32)
+}
+PC6001_8BIT_CHARMAP_COMPAT = {
+    unicodedata.normalize("NFKD", key): value
+    for key, value in PC6001_8BIT_CHARMAP.items()
+    if unicodedata.normalize("NFKD", key) != key
+}
+
+
+def encode_pc6001_8bit_charset(s, try_harder=True):
+    s = "".join(
+        [
+            (
+                unicodedata.normalize("NFKD", s[i : i + 1])
+                if (
+                    unicodedata.name(s[i : i + 1], "?")
+                    .lower()
+                    .startswith("hiragana letter")
+                    or unicodedata.name(s[i : i + 1], "?")
+                    .lower()
+                    .startswith("katakana letter")
+                )
+                else s[i : i + 1]
+            )
+            for i in range(len(s))
+        ]
+    )
+    byts, chars_consumed, num_chars = b"", 0, len(s)
+    while chars_consumed < num_chars:
+        ch = s[chars_consumed]
+        byt = PC6001_8BIT_CHARMAP.get(ch, PC6001_8BIT_CHARMAP_COMPAT.get(ch)) or (
+            bytes([ord(ch)]) if ord(ch) <= 0x7F else None
+        )
+        if byt is None and try_harder:
+            cch = unicodedata.normalize("NFKD", ch)
+            byt = PC6001_8BIT_CHARMAP.get(cch, PC6001_8BIT_CHARMAP_COMPAT.get(cch)) or (
+                bytes([ord(cch)]) if ord(cch) <= 0x7F else None
+            )
+        if byt is None:
+            raise UnicodeEncodeError(
+                "pc6001-8bit",
+                s,
+                chars_consumed,
+                chars_consumed + 1,
+                f"no mapping for U+{ord(ch):04X} {unicodedata.name(ch, repr(ch))}",
+            )
+        byts += byt
+        chars_consumed += 1
+    return byts
+
+
+def decode_pc6001_8bit_charset(byts, preserve=MINIMAL_CONTROLS):
+    s, bytes_consumed, num_bytes = "", 0, len(byts)
+    while bytes_consumed < num_bytes:
+        byt = byts[bytes_consumed]
+        if (
+            bytes_consumed > 0
+            and byts[bytes_consumed - 1] == 0x14
+            and byt >= 0x30
+            and byt <= 0x4F
+        ):
+            s = (
+                s[: -len(PC6001_8BIT_CHARSET[0x14])]
+                + PC6001_8BIT_ALTCHARSET[byt - 0x30]
+            )
+        elif byt in preserve:
+            s += chr(byt)
+        else:
+            s += PC6001_8BIT_CHARSET[byt]
+        if (
+            len(s) > 1
+            and s[-1:]
+            in "\N{HALFWIDTH KATAKANA VOICED SOUND MARK}\N{HALFWIDTH KATAKANA SEMI-VOICED SOUND MARK}"
+            and unicodedata.name(s[-2:-1], "?").lower().startswith("hiragana letter")
+        ):
+            s = s[:-2] + unicodedata.normalize("NFKC", s[-2:])
+        bytes_consumed += 1
+    round_trip_byts = encode_pc6001_8bit_charset(s)
+    assert byts == round_trip_byts, UnicodeDecodeError(
+        "pc6001-8bit",
+        byts,
+        0,
+        num_bytes,
+        f"round-trip failure for {repr(s)} with preserve={repr(preserve)}; result:\n {repr(byts)}, got:\n {repr(round_trip_byts)}",
+    )
+    return s
+
+
 def main():
-    _, infn = (  # usage: python nontama_to_bload.py INPUT.p6  ## writes OUTPUT_start_stop_exe.bin
+    _, infn = (  # usage: python nontama_to_bload.py INPUT.p6  ## writes OUTPUT[_loadNN]_start_stop_exe.bin
         sys.argv
     )
     assert os.path.exists(infn)
     p6_in = open(infn, "rb").read()
-    bload_out, start_addr, stop_addr, exe_addr = nontama_to_bload(p6_in)
-    outfn = f"{os.path.splitext(os.path.basename(infn))[0]}_{start_addr:04x}_{stop_addr:04x}_{exe_addr:04x}.bin"
-    if os.path.exists(outfn):
-        os.remove(outfn)
-        print(f"Removed old {outfn}")
-    print(f"Writing {outfn}")
-    open(outfn, "wb").write(bload_out)
+    results = []
+    while True:
+        bload_out, load_name, start_addr, stop_addr, exe_addr, p6_in = nontama_to_bload(p6_in)
+        results.append(dict(bload_out=bload_out, load_name=load_name, start_addr=start_addr, stop_addr=stop_addr, exe_addr=exe_addr))
+        if not NONTAMA_HEADER_START in p6_in:
+            break
+    for i, result in enumerate(results):
+        bload_out, load_name, start_addr, stop_addr, exe_addr = result['bload_out'], result['load_name'], result['start_addr'], result['stop_addr'], result['exe_addr']
+        load_suffix = '' if len(results) == 1 else f"_load{1 + i:02d}"
+        if load_name is not None:
+            load_name_unicode = decode_pc6001_8bit_charset(load_name)
+            load_name_fs_safe = ''
+            for i, ch in enumerate(load_name_unicode):
+                if ch in set('"*+,/:;<=>?[\\]|\x7f¥¦') | set(chr(i) for i in range(0x20)):
+                    ch = "_"
+                load_name_fs_safe += ch
+            load_suffix = f"_{load_name_fs_safe}" + load_suffix
+        outfn = f"{os.path.splitext(os.path.basename(infn))[0]}{load_suffix}_{start_addr:04x}_{stop_addr:04x}_{exe_addr:04x}.bin"
+        if os.path.exists(outfn):
+            os.remove(outfn)
+            print(f"Removed old {outfn}")
+        print(f"Writing {outfn}")
+        open(outfn, "wb").write(bload_out)
 
 
 if __name__ == "__main__":
